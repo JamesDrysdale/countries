d78f8ccc06e2c4049abff6b536020e5799e93290
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-05-12 15:47:32 +0100

    Can see list of countries and find one by name

--- src/app.js
+++ src/app.js
@@ -1,6 +1,25 @@
 import Vue from 'vue';
+
 document.addEventListener('DOMContentLoaded', () => {
     new Vue({
-        el: "#app",
-})
-});
+      el: '#app',
+      data: {
+              countries: [],
+              filterCountry: ""
+      },
+      mounted(){
+          this.fetchCountries();
+           this.filteredCountries();
+        },
+      methods: {
+          fetchCountries: function(){
+              fetch("https://restcountries.eu/rest/v2/all")
+                .then(response =>  response.json())
+                .then(countries => this.countries = countries)
+            },
+          filteredCountries: function(){
+              return this.countries.filter(country => country.name === this.filterCountry)
+            }
+      }
+    });
+  });
